46668e0a652273ed1aa13076e578cae337e5affe
qkffleostm <qkffleostm>
2023-04-29 12:41:54 +0900

    최종 커밋

--- src/components/LocalPage.jsx
+++ src/components/LocalPage.jsx
@@ -1,36 +1,59 @@
 import axios from 'axios';
 import React from 'react';
-import {Row, Col, Table} from 'react-bootstrap';
+import {Row, Col, Table, Form, Button} from 'react-bootstrap';
 import {useEffect} from 'react';
 import { useState } from 'react';
+import MapPage from './MapPage';
 
 const LocalPage = () => {
     const [locals, setLocals] = useState([]);
+    const [query, setQuery] = useState('인하대학교');
+    const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [is_end, setIs_end] = useState(false);
+
     const getLocal = async() => {
         const url = "https://dapi.kakao.com/v2/local/search/keyword.json"
         const config = {
             headers: {"Authorization": "KakaoAK b80880fbde422de3fd9b4a4e67c9bb54"},
-            params: {query: "인하대", page:1, size:5}
+            params: {query: query, page: page, size:5}
         }
         const result= await axios.get(url, config);
         console.log(result);
         setLocals(result.data.documents);
+        setTotal(result.data.meta.pageable_count);
+        setIs_end(result.data.meta.is_end);
     }
 
     useEffect(()=>{
         getLocal();
-    }, []);
+    }, [page]);
 
+    const onSearch = (e) => {
+        e.preventDefault();
+        getLocal();
+    }
     return (
         <Row>
             <Col>
                 <h1 className='text-center my-5'>지역검색</h1>
+                <Row className='my-2'>
+                    <Col md={3}>
+                        <Form onSubmit={onSearch}>
+                            <Form.Control placeholder='검색어' value={query} onChange={(e)=>setQuery(e.target.value)}></Form.Control>
+                        </Form>
+                    </Col>
+                    <Col>   
+                        검색수 {total}
+                    </Col>
+                </Row>
                 <Table striped bordered hover>
                     <thead>
                         <tr className='text-center'>
                             <td>장소명</td>
                             <td>주소</td>
                             <td>전화</td>
+                            <td>위치</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -39,9 +62,15 @@ const LocalPage = () => {
                                 <td>{local.place_name}</td>
                                 <td>{local.phone}</td>
                                 <td>{local.address_name}</td>
+                                <td><MapPage local={local}/></td>
                             </tr>)}
                     </tbody>
                 </Table>
+                <div className='text-center my-2'>
+                    <Button disabled={page==1} onClick={()=>setPage(page-1)}>이전</Button>
+                    <span className="mx-3">{page}</span>
+                    <Button disabled={is_end} onClick={()=>setPage(page+1)}>다음</Button>
+                </div>
             </Col>
         </Row>
     )
